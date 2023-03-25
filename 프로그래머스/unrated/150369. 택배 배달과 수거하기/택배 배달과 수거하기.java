e08395ac384e1ed414c0a9f2684e9428c9e26233
H4R1B0 <qkrguswns25@naver.com>
2023-03-25 15:55:40 +0900

    [unrated] Title: 택배 배달과 수거하기, Time: 2.67 ms, Memory: 89.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        long answer = 0;
+        int delivered = 0, pickuped = 0;
+        for(int i=n-1;i>=0;i--){
+            delivered += deliveries[i];
+            pickuped += pickups[i];
+            
+            while(delivered > 0 || pickuped > 0){
+                delivered -= cap;
+                pickuped -= cap;
+                answer += (i+1)*2;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
