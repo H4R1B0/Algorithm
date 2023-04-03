6c2220ef8b40214709b69f3bb3a34af71e80c7fd
H4R1B0 <qkrguswns25@naver.com>
2023-04-03 15:28:05 +0900

    [Platinum V] Title: 히스토그램, Time: 288 ms, Memory: 26228 KB -BaekjoonHub

---
+++ 백준/Platinum/1725. 히스토그램/히스토그램.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static long[] arr = new long[100010];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++)
+            arr[i] = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        stack.add(0);
+        long answer = 0;
+        for (int i = 1; i <= N + 1; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+                int idx = stack.pop();
+                answer = Math.max(answer, arr[idx] * (i - stack.peek() - 1));
+            }
+            stack.add(i);
+        }
+        System.out.println(answer);
+    }
+}
