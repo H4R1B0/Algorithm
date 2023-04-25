137eb52fad7a5c6c83c2547c640f6a74000c2d86
H4R1B0 <qkrguswns25@naver.com>
2023-04-25 23:24:58 +0900

    [Gold IV] Title: 뱀, Time: 140 ms, Memory: 14480 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -1,90 +1,125 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] xd = {-1, 0, 1, 0};
-    static int[] yd = {0, 1, 0, -1};
-    static int N;
-    static int[][] board;
-    //    static int[][] tmp;
-    static List<Dir> arr = new LinkedList<>();
+    //우하좌상
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        board = new int[N][N];
-        int K = Integer.parseInt(br.readLine());
-        String[] tmpSplit = null;
-        for (int i = 0; i < K; i++) {
-            tmpSplit = br.readLine().split(" ");
-            int x = Integer.parseInt(tmpSplit[0]);
-            int y = Integer.parseInt(tmpSplit[1]);
-            board[x - 1][y - 1] = 1;
-        }
-        int L = Integer.parseInt(br.readLine());
-        for (int i = 0; i < L; i++) {
-            tmpSplit = br.readLine().split(" ");
-            int time = Integer.parseInt(tmpSplit[0]);
-            char dir = tmpSplit[1].charAt(0);
-            arr.add(new Dir(time, dir));
-        }
-
-        System.out.println(getAnswer());
-    }
-
-    static int getAnswer() {
-        int x = 0, y = 0, dir = 1, time = 0, idx = 0;
-        Deque<Pos> dq = new LinkedList<>();
-        dq.add(new Pos(x, y));
-        board[x][y] = 2;
-        while (true) {
-            int nx = x + xd[dir];
-            int ny = y + yd[dir];
-            time++;
-
-            if (nx < 0 || nx >= N || ny < 0 || ny >= N || board[nx][ny] == 2)
-                break;
-            
-            dq.addFirst(new Pos(nx, ny));
-            if (board[nx][ny] == 0) {
-                board[dq.peekLast().x][dq.peekLast().y] = 0;
-                dq.pollLast();
-            }
-            board[nx][ny] = 2;
-
-            if (idx < arr.size()) {
-                if (time == arr.get(idx).time) {
-                    if (arr.get(idx).dir == 'L')
-                        dir = (dir + 3) % 4;
-                    else
-                        dir = (dir + 1) % 4;
-                    idx++;
-                }
-            }
-            x=nx;
-            y=ny;
-        }
-        return time;
-    }
-
-    static class Pos {
+    static class Node {
         int x, y;
 
-        Pos(int x, int y) {
+        public Node(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
 
-    static class Dir {
-        int time;
-        char dir;
+    static int N, K, L;
+    static boolean[][] apple;
+    static Map<Integer, String> moveInfo;
+    static Deque<Node> snake;
+    static int x, y, dir;
 
-        Dir(int time, char dir) {
-            this.time = time;
-            this.dir = dir;
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine());
+        apple = new boolean[N][N];
+
+        for (int i = 0; i < K; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            apple[r - 1][c - 1] = true;
         }
+
+        L = Integer.parseInt(br.readLine());
+        moveInfo = new HashMap<>();
+        for (int i = 0; i < L; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int time = Integer.parseInt(st.nextToken());
+            String dir = st.nextToken();
+
+            moveInfo.put(time, dir);
+        }
+        //입력 완료
+
+        x = 0;
+        y = 0;
+        dir = 0;
+        snake = new ArrayDeque<>();
+        snake.addFirst(new Node(x, y)); //뱀의 초기 위치 넣어주기
+
+        int time = 0; //출력 할 시간
+        while (true) {
+            //1.시간 +1
+            time++;
+
+            //2. 뱀 이동
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+
+            //3. 종료 조건 충족 시 종료
+            if (isBreak(nx, ny)) {
+                break;
+            }
+
+            //4. 사과 유무 처리
+            snake.addFirst(new Node(nx, ny));
+            if (apple[nx][ny]) { //사과가 있다면
+                apple[nx][ny] = false;
+            } else { //사과가 없다면
+                snake.pollLast();
+            }
+
+            //5. 현재 시간에 방향을 변경해야 하는 경우 방향 변경
+            if (moveInfo.containsKey(time)) {
+                String newD = moveInfo.get(time);
+                changeDir(newD);
+            }
+
+            //6. 방문 처리, 값 변경
+            x = nx;
+            y = ny;
+        }
+
+        System.out.println(time);
+    }
+
+    //방향 변경
+    private static void changeDir(String newD) {
+        if (newD.equals("D")) {
+            dir = (dir + 1) % 4;
+        } else if (newD.equals("L")) {
+            dir -= 1;
+            if (dir == -1) dir = 3;
+        }
+    }
+
+    //종료 조건 처리
+    private static boolean isBreak(int x, int y) {
+        if (x < 0 || x >= N || y < 0 || y >= N) {
+            return true;
+        }
+
+        //자기 몸에 닿았을 때
+        int size = snake.size();
+        for (int i = 0; i < size; i++) {
+            Node tmp = snake.pollFirst();
+            if (tmp.x == x && tmp.y == y)
+                return true;
+            snake.addLast(tmp);
+        }
+        return false;
     }
 }
