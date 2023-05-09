ed28a8a8b1a89478f2dc27d05d0406bbbee2b9b7
H4R1B0 <qkrguswns25@naver.com>
2023-05-09 22:51:51 +0900

    [Gold II] Title: 상어 중학교, Time: 184 ms, Memory: 20868 KB -BaekjoonHub

--- 백준/Gold/21609. 상어 중학교/상어 중학교.java
+++ 백준/Gold/21609. 상어 중학교/상어 중학교.java
@@ -1,225 +1,195 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	static class Node{
-		int x, y;
+    static int N, M, answer;
+    static int[] xd = {0, 0, 1, -1};
+    static int[] yd = {1, -1, 0, 0};
+    static int[][] board;
+    static boolean[][] visit;
 
-		public Node(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        board = new int[N][N];
+        visit = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-		@Override
-		public String toString() {
-			return "Node [x=" + x + ", y=" + y + "]";
-		}
+        while (true) {
+            //가장 큰 블록 찾기
+            Block result = getBigBlock();
 
-	}
+            if (result.blocks.size() < 2)
+                break;
+            //블록 삭제
+            deleteBlock(result);
+            //중력
+            gravity();
+            //회전
+            rotate();
+            //중력
+            gravity();
+        }
+        System.out.println(answer);
+    }
 
-	static int N, maxCnt, result;
-	static int[] dx = {-1, 0, 1, 0};
-	static int[] dy = {0, 1, 0, -1};
-	static int[][] blocks;
-	static Node standard;
-	static Queue<Node> queue;
-	static Queue<Node> group;
-	static Queue<Node> rainbow;
-	static boolean[][] visited;
+    static Block getBlock(int a, int b) {
+        List<int[]> blocks = new ArrayList<>();
+        List<int[]> normalBlocks = new ArrayList<>();
+        Queue<int[]> rainbowBlocks = new LinkedList<>();
+        Queue<int[]> q = new LinkedList<>();
+        blocks.add(new int[]{a, b});
+        normalBlocks.add(new int[]{a, b});
+        q.add(new int[]{a, b});
+        visit[a][b] = true;
+        int rainbowCount = 0; //rainbow count
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		blocks = new int[N][N];
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
 
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<N; j++) {
-				blocks[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+            for (int i = 0; i < 4; i++) {
+                int nx = x + xd[i];
+                int ny = y + yd[i];
 
-		//1. 블록 그룹 찾기
-		//1-1. 검정색 블록, 무지개블록 pass
-		while(true) {
-			visited = new boolean[N][N];
-			maxCnt = -1;
-			queue = new ArrayDeque<>();
-			group = new ArrayDeque<>();
-			rainbow = new ArrayDeque<>();
-			standard = new Node(N, N);
-			for(int i=0; i<N; i++) {
-				for(int j=0; j<N; j++) {
-					if(visited[i][j]) continue;
-					if(blocks[i][j] != -1 && blocks[i][j] != 0 && blocks[i][j] != -2) {
-						findGroup(i, j);
-					}
-				}
-			}
-			if(group.size() < 2) break;
+                if (!isInRange(nx, ny) || visit[nx][ny])
+                    continue;
 
-//		System.out.println(group);
+                //rainbow
+                if (board[nx][ny] == 0) {
+                    visit[nx][ny] = true;
+                    rainbowBlocks.add(new int[]{nx, ny});
+                    rainbowCount++;
+                    blocks.add(new int[]{nx, ny});
+                    q.add(new int[]{nx, ny});
+                }
+                //normal block
+                else if (board[nx][ny] == board[a][b]) {
+                    visit[nx][ny] = true;
+                    blocks.add(new int[]{nx, ny});
+                    q.add(new int[]{nx, ny});
+                    normalBlocks.add(new int[]{nx, ny});
+                }
+            }
+        }
 
-			//2. 큐에 저장된 블록 모두 -2로 셋팅
-			//2-1. 점수 추가
-			result += Math.pow(group.size(), 2);
-			for(Node node : group) {
-				blocks[node.x][node.y] = -2;
-			}
+        while (!rainbowBlocks.isEmpty()){
+            visit[rainbowBlocks.peek()[0]][rainbowBlocks.peek()[1]] = false;
+            rainbowBlocks.poll();
+        }
 
-			//3. 중력 작용(완탐 -> 큰 행부터시작)
-			//3-1. 아래 행이 비어있으면 옮김(검정색 블록  continue)
-			moveBlock();
-//			System.out.println("-------");
-//			for(int i=0; i<N; i++) {
-//				System.out.println(Arrays.toString(blocks[i]));
-//			}
-//			System.out.println();
+        Collections.sort(normalBlocks, (o1, o2) -> {
+            if (o1[0] == o2[0])
+                return o1[1] - o2[1];
+            return o1[0] - o2[0];
+        });
 
-			//4. 90도 반시계 이동(배열 하나 만들어서 저장)
-			rotate90();
+        Block block = new Block();
+        block.x = normalBlocks.get(0)[0];
+        block.y = normalBlocks.get(0)[1];
+        block.rainbowCount = rainbowCount;
+        block.blocks = blocks;
+        return block;
+    }
 
-//			for(int i=0; i<N; i++) {
-//				System.out.println(Arrays.toString(blocks[i]));
-//			}
-//			System.out.println();
+    static Block getBigBlock() {
+        visit = new boolean[N][N];
+        Block result = new Block();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visit[i][j]) {
+                    if (board[i][j] >= 1 && board[i][j] <= M) {
+                        Block tmp = getBlock(i, j);
 
-			//5. (3)중력 작용
-			moveBlock();
+                        if (result.blocks.size() == 0) {
+                            result = tmp;
+                        } else {
+                            if (isBigBlock(result, tmp))
+                                result = tmp;
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
 
-//			for(int i=0; i<N; i++) {
-//				System.out.println(Arrays.toString(blocks[i]));
-//			}
-//			System.out.println();
-//			System.out.println("result "+result);
-		}
-		System.out.println(result);
+    static void gravity() {
+        for (int i = N - 2; i >= 0; i--) {
+            for (int j = 0; j < N; j++) {
+                if (board[i][j] == -2 || board[i][j] == -1)
+                    continue;
 
-		br.close();
-	}
+                int color = board[i][j];
+                int nx = i + 1;
+                while (true) {
+                    if (nx == N)
+                        break;
+                    if (board[nx][j] != -2) {
+                        break;
+                    }
+                    nx++;
+                }
+                nx--;
+                board[i][j] = -2;
+                board[nx][j] = color;
+            }
+        }
+    }
 
-	private static void rotate90() {
-		int[][] temp = new int[N][N];
+    static void rotate() {
+        int[][] tmpBoard = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                tmpBoard[i][j] = board[j][N - 1 - i];
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                board[i][j] = tmpBoard[i][j];
+            }
+        }
+    }
 
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				temp[N-1-j][i] = blocks[i][j];
-			}
-		}
+    static void deleteBlock(Block result) {
+        List<int[]> blocks = result.blocks;
+        for (int i = 0; i < blocks.size(); i++) {
+            int x = blocks.get(i)[0];
+            int y = blocks.get(i)[1];
+            board[x][y] = -2;
+        }
+        answer += (blocks.size() * blocks.size());
+    }
 
-		for(int i=0; i<N; i++) {
-			blocks[i] = temp[i].clone();
-		}
+    static boolean isBigBlock(Block a, Block b) {
+        if (a.blocks.size() == b.blocks.size()) {
+            if (a.rainbowCount == b.rainbowCount) {
+                if (a.x == b.x)
+                    return a.y < b.y;
+                return a.x < b.x;
+            }
+            return a.rainbowCount < b.rainbowCount;
+        }
+        return a.blocks.size() < b.blocks.size();
+    }
 
-	}
+    static boolean isInRange(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < N;
+    }
 
-	//3. 중력 작용(완탐 -> 큰 행부터시작)
-	//3-1. 아래 행이 비어있으면 옮김(검정색 블록 continue)
-	//!!!!!!!!!!!!!!!!!1
-	private static void moveBlock() {
-		//빈공간, 데이터 있는 공간 찾기
-		//1. 빈공간 저장
-		//2. 검은 블록 만나면 초기화
-		//3. 일반 블록인 경우, 빈 공간에 저장
-		Queue<Node> space = new ArrayDeque<>();
-		for(int i=0; i<N; i++) {    //열 단위로 보기
-			for(int j=N-1; j>=0; j--) {
-				if(blocks[j][i] == -2) space.offer(new Node(j, i));
-				else if(blocks[j][i] == -1) space.clear();
-				else {
-					if(!space.isEmpty()) {
-						Node node = space.poll();
-						blocks[node.x][node.y] = blocks[j][i];
-						blocks[j][i] = -2;
-						space.offer(new Node(j, i)); //!!!!!!!!!중요, 현재 위치가 빈공간이 되므로
-					}
-				}
-			}
-			space.clear();
-		}
-	}
-
-
-	//1-2. 일반 블록 : 해당 번호와 0인 경우 상하좌우로 찾음(검정색 블록 or 다른 번호 블록 pass)
-	//	기준 블록, 블록 수(무지개블록 수), 블록 정보(큐에 저장)
-	//	무지개블록 저장(큐) -> 다음 블록에서 찾을 때, 방문처리 해제
-	private static void findGroup(int x, int y) {
-		Queue<Node> nQueue = new ArrayDeque<>();
-		Queue<Node> nGroup = new ArrayDeque<>();
-		Queue<Node> nRainbow = new ArrayDeque<>();
-		int blockNum = blocks[x][y];
-		Node nStandard = new Node(x, y);
-
-		visited[x][y] = true;
-		nQueue.offer(new Node(x, y));
-		nGroup.offer(new Node(x, y));
-
-		while(!nQueue.isEmpty()) {
-			Node node = nQueue.poll();
-			int px = node.x;
-			int py = node.y;
-
-			for(int i=0; i<4; i++) {
-				int nx = px + dx[i];
-				int ny = py + dy[i];
-
-				if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-				if(visited[nx][ny]) continue;
-
-				//일반 블록의 number가 같은 경우
-				if(blocks[nx][ny] == blockNum) {
-					if(nx < nStandard.x) nStandard = new Node(nx, ny);
-					else if(nx == nStandard.x && ny < nStandard.y) {
-						nStandard = new Node(nx, ny);
-					}
-					nQueue.offer(new Node(nx, ny));
-					nGroup.offer(new Node(nx, ny));
-					visited[nx][ny] = true;
-				} else if(blocks[nx][ny] == 0) {    //무지개 블록인 경우
-					nQueue.offer(new Node(nx, ny));
-					nGroup.offer(new Node(nx, ny));
-					nRainbow.offer(new Node(nx, ny));    //추가
-					visited[nx][ny] = true;
-				}
-			}
-		}
-
-		//무지개블록 방문 false처리
-		for(Node node : nRainbow) {
-			visited[node.x][node.y] = false;
-		}
-
-		//https://www.acmicpc.net/board/view/110216
-		//블록 그룹 찾기
-		if(maxCnt < nGroup.size()) {    //그룹의 크기
-			update(nGroup, nRainbow, nStandard);
-		} else if(maxCnt == nGroup.size()) {
-			if(rainbow.size() < nRainbow.size()) {    //무지개 블록 크기
-				update(nGroup, nRainbow, nStandard);
-			} else if(rainbow.size() == nRainbow.size()) {
-				if(standard.x < nStandard.x) {    //기준 블록 행의 크기
-					update(nGroup, nRainbow, nStandard);
-				} else if(standard.x == nStandard.x) {
-					if(standard.y < nStandard.y) {    //기중 블록 열의 크기
-						update(nGroup, nRainbow, nStandard);
-					}
-				}
-			}
-		}
-	}
-
-
-	private static void update(Queue<Node> nGroup, Queue<Node> nRainbow, Node nStandard) {
-		maxCnt = nGroup.size();
-		group = nGroup;
-		rainbow = nRainbow;
-		standard = nStandard;
-	}
+    //블록 클래스
+    static class Block {
+        int x, y, rainbowCount;
+        List<int[]> blocks = new ArrayList<>();
+    }
 }
