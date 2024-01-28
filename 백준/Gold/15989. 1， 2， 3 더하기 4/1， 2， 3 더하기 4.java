833bae01a5a02e5023993f5b1db2c1b7060747d4
H4R1B0 <qkrguswns25@naver.com>
2024-01-28 21:05:05 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 128 ms, Memory: 14612 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        //입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder answer = new StringBuilder();
+        int[][] dp = new int[10001][4];
+        dp[1][1] = 1;
+        dp[2][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+        for (int i = 4; i < 10001; i++) {
+            dp[i][1] = dp[i - 1][1];
+            dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
+            dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
+        }
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            answer.append(dp[n][1] + dp[n][2] + dp[n][3]).append('\n');
+        }
+        System.out.println(answer);
+    }
+}
