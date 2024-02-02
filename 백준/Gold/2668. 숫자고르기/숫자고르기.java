289486717f2201a78a1539db3cb266a3fc087c93
H4R1B0 <qkrguswns25@naver.com>
2024-02-02 14:36:35 +0900

    [Gold V] Title: 숫자고르기, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -1,43 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int N, idx;
-    static int[] numbers;
-    static int[] answer;
+    static int[] nums, answer;
     static boolean[] visit;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        numbers = new int[N + 1];
+        nums = new int[N + 1];
         answer = new int[N];
-        visit = new boolean[N+1];
+        visit = new boolean[N + 1];
         for (int i = 1; i <= N; i++) {
-            numbers[i] = Integer.parseInt(br.readLine());
+            nums[i] = Integer.parseInt(br.readLine());
         }
-
         for (int i = 1; i <= N; i++) {
-            Arrays.fill(visit,false);
-            dfs(i,i);
+            int num = nums[i];
+            Arrays.fill(visit, false);
+            dfs(i, i);
         }
         System.out.println(idx);
-        for(int i=0;i<idx;i++){
+        for (int i = 0; i < idx; i++) {
             System.out.println(answer[i]);
         }
     }
 
-    static void dfs(int start, final int end) {
-        if(visit[start]){
-            if(start == end){
+    static void dfs(int start, int end) {
+        if (visit[start]) {
+            if (start == end) {
                 answer[idx++] = end;
             }
             return;
-        }else{
+        } else {
             visit[start] = true;
-            dfs(numbers[start], end);
+            dfs(nums[start], end);
         }
     }
 }
