dcf5c8d9934e7e257048017fae2fe7ad9f978095
H4R1B0 <qkrguswns25@naver.com>
2024-02-12 19:25:11 +0900

    [Bronze III] Title: Baseball, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10214. Baseball/Baseball.py
@@ -0,0 +1,14 @@
+T = int(input())
+for _ in range(T):
+    A=0
+    B=0
+    for _ in range(9):
+        a,b = map(int, input().split())
+        A+=a
+        B+=b
+    if A>B:
+        print('Yonsei')
+    elif A<B:
+        print('Korea')
+    else:
+        print('Draw')
