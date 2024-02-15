6af77f0871a7b11caff9c5aa103097d228c85917
H4R1B0 <qkrguswns25@naver.com>
2024-02-15 21:01:40 +0900

    [Gold II] Title: 새로운 게임 2, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold II/17837. 새로운 게임 2/새로운 게임 2.py
+++ 백준/Gold II/17837. 새로운 게임 2/새로운 게임 2.py
@@ -1,49 +1,47 @@
 import sys
 
-xd = [0, 0, -1, 1]
-yd = [1, -1, 0, 0]
+xd = [0,0,-1,1]
+yd = [1,-1,0,0]
 
 def move(chess_num):
-    x, y, dir = horse[chess_num]
-    nx = x+xd[dir]
-    ny = y+yd[dir]
+    x,y,d = horse[chess_num]
+    nx = x+xd[d]
+    ny = y+yd[d]
 
     if not 0<=nx<n or not 0<=ny<n or board[nx][ny]==2:
-        if dir in [0,1]:
-            ndir = 1-dir
+        if d in [0,1]:
+            nd = 1-d
         else:
-            ndir = 5-dir
-        horse[chess_num][2] = ndir
-        nx = x+xd[ndir]
-        ny = y+yd[ndir]
+            nd = 5-d
+        horse[chess_num][2]=nd
+        nx = x+xd[nd]
+        ny = y+yd[nd]
         if not 0<=nx<n or not 0<=ny<n or board[nx][ny]==2:
             return False
 
     horse_tmp = []
-    idx = chess_map[x][y].index(chess_num)
-    horse_tmp.extend(chess_map[x][y][idx:])
-    chess_map[x][y] = chess_map[x][y][:idx]
+    idx = chess[x][y].index(chess_num)
+    horse_tmp.extend(chess[x][y][idx:])
+    chess[x][y] = chess[x][y][:idx]
 
     if board[nx][ny]==1:
         horse_tmp = horse_tmp[-1::-1]
-
+        
     for i in horse_tmp:
-        chess_map[nx][ny].append(i)
+        chess[nx][ny].append(i)
         horse[i][:2] = [nx,ny]
-    if len(chess_map[nx][ny])>=4:
-        return True
-    return False
+        
+    return len(chess[nx][ny])>=4
+        
 
 n, k = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
-# print(board)
-chess_map = [[[] for _ in range(n)] for _ in range(n)]
+chess = [[[] for _ in range(n)] for _ in range(n)]
 horse = [0]*k
-
 for i in range(k):
-    x, y, dir = map(int, input().split())
-    chess_map[x-1][y-1].append(i)
-    horse[i] = [x-1, y-1, dir-1]
+    x,y,d = map(int, input().split())
+    horse[i] = [x-1,y-1,d-1]
+    chess[x-1][y-1].append(i)
 
 cnt = 1
 while cnt<=1000:
