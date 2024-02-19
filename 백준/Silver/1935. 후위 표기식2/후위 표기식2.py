836ce3a4d0143f45bbfb51fc700942442b5ecb39
H4R1B0 <qkrguswns25@naver.com>
2024-02-19 17:17:56 +0900

    [Silver III] Title: 후위 표기식2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1935. 후위 표기식2/후위 표기식2.py
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.py
@@ -5,19 +5,10 @@ for i in range(n):
     num[i] = int(input())
 stack = []
 for i in arr:
-    # print(stack)
     if 'A'<=i<='Z':
         stack.append(num[ord(i)-ord('A')])
     else:
         b = stack.pop()
         a = stack.pop()
-
-        if i =='+' :
-            stack.append(a+b)
-        elif i == '-' :
-            stack.append(a-b)
-        elif i == '*' :
-            stack.append(a*b)
-        elif i == '/' :
-            stack.append(a/b)
+        stack.append(eval(''.join(str(a)+i+str(b))))
 print('%.2f' %stack[0])
