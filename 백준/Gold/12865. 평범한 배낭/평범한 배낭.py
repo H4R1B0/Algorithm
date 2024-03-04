534a0831d84e914106f8bfa91e542410ca07c2d8
H4R1B0 <qkrguswns25@naver.com>
2024-03-04 18:14:52 +0900

    [Gold V] Title: 평범한 배낭, Time: 4560 ms, Memory: 229084 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,14 @@
+n,k = map(int,input().split())
+arr = [[0]*2 for _ in range(n+1)]
+for i in range(1,n+1):
+    arr[i] = list(map(int,input().split()))
+# print(arr)
+dp = [[0]*(k+1) for _ in range(n+1)]
+for i in range(1,n+1):
+    w,v = arr[i]
+    for j in range(1,k+1):
+        if j<w:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w]+v)
+print(max(dp[-1]))
