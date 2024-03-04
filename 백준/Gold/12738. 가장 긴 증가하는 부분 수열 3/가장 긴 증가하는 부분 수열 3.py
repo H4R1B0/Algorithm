eff5fd7601ce4619f839ee0b41202b8e06581614
H4R1B0 <qkrguswns25@naver.com>
2024-03-04 21:44:53 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 976 ms, Memory: 151248 KB -BaekjoonHub

--- 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
@@ -1,23 +1,13 @@
-def binary_search(left,right,target):
-    while left<right:
-        mid = (left+right)//2
-        if res[mid]==target:
-            return mid
-        elif res[mid] < target:
-            left = mid+1
-        else:
-            right = mid
-    return left
+from bisect import bisect_left
 
 n = int(input())
 arr = list(map(int,input().split()))
-res = [arr[0]]
+res = []
 
-for i in range(1,len(arr)):
-    if res[-1]<arr[i]:
-        res.append(arr[i])
+for num in arr:
+    idx = bisect_left(res,num)
+    if len(res)==idx:
+        res.append(num)
     else:
-        idx = binary_search(0,len(res)-1,arr[i])
-        # print(idx)
-        res[idx] = arr[i]
+        res[idx]=num
 print(len(res))
