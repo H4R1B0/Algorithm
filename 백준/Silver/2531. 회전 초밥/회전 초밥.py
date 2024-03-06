0c0e748f67edd8b56d9520b86aa35f3889c09631
H4R1B0 <qkrguswns25@naver.com>
2024-03-06 14:32:17 +0900

    [Silver I] Title: 회전 초밥, Time: 1228 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -2,14 +2,27 @@ from collections import Counter
 
 n,d,k,c = map(int,input().split())
 arr = [int(input()) for _ in range(n)]
-arr = arr+arr[:-1]
-# print(arr)
-answer = 0
-for i in range(n):
-    tmp = dict(Counter(arr[i:i+k]))
-    # print(len(tmp))
-    if c in tmp:
-        answer = max(answer,len(tmp))
+cnt = {}
+for num in arr[:k]:
+    if num not in cnt:
+        cnt[num]=0
+    cnt[num]+=1
+# print(len(cnt))
+if c not in cnt:
+    answer = len(cnt)+1
+else:
+    answer = len(cnt)
+for i in range(1,n):
+    e = (i+k-1)%n
+    if arr[e] not in cnt:
+        cnt[arr[e]]=0
+    cnt[arr[e]]+=1
+    cnt[arr[i-1]]-=1
+    if cnt[arr[i-1]]==0:
+        del cnt[arr[i-1]]
+    if c not in cnt:
+        answer = max(answer,len(cnt)+1)
     else:
-        answer = max(answer,len(tmp)+1)
+        answer = max(answer,len(cnt))
+    # print(cnt)
 print(answer)
