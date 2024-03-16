d84a1676e1e0a4e1dffea3ac96e95b262e5ce167
H4R1B0 <qkrguswns25@naver.com>
2024-03-16 16:04:10 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
@@ -1,20 +1,8 @@
 def f1(n):
-    global answer1
-    if n==1 or n==2:
-        answer1+=1
-        return 1
-    return f1(n-1)+f1(n-2)
+    a,b = 1,1
+    for _ in range(3,n+1):
+        a,b = b,a+b
+    return b
 
-def f2(n):
-    global answer2
-    arr = [0]*42
-    arr[1] = 1
-    arr[2] = 1
-    for i in range(3,n+1):
-        arr[i] = arr[i-1]+arr[i-2]
-        answer2+=1
-answer1,answer2 = 0,0
 n = int(input())
-f1(n)
-f2(n)
-print(answer1, answer2)
+print(f1(n), n-2)
