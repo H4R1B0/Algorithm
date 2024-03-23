5baa08565861b0cdfcdf24642ddfc9b711a662df
H4R1B0 <qkrguswns25@naver.com>
2024-03-23 16:45:53 +0900

    [Silver III] Title: 돌 게임 3, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.py
@@ -0,0 +1,11 @@
+n = int(input())
+dp = [0,1,0,1,1]
+for i in range(5,n+1):   
+    if dp[i-1] and dp[i-3] and dp[i-4]:
+        dp.append(0)
+    else :
+        dp.append(1)
+if dp[n]:
+    print('SK')
+else:
+    print('CY')
