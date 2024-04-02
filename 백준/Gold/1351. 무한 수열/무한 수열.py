cb8deedce1072c2dd5c79403756bb689cd4933d5
H4R1B0 <qkrguswns25@naver.com>
2024-04-02 12:47:51 +0900

    [Gold V] Title: 무한 수열, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1351. 무한 수열/무한 수열.py
@@ -0,0 +1,10 @@
+def dfs(n):
+    if n in dic:
+        return dic[n]
+    dic[n] = dfs(n//p)+dfs(n//q)
+    return dic[n]
+
+n,p,q = map(int,input().split())
+dic = dict()
+dic[0] = 1
+print(dfs(n))
