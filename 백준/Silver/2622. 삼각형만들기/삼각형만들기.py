8e4da7f5aa351dc02eca140e3327d12c0e4d4e06
H4R1B0 <qkrguswns25@naver.com>
2024-05-29 20:06:14 +0900

    [Silver V] Title: 삼각형만들기, Time: 740 ms, Memory: 110616 KB -BaekjoonHub

---
+++ 백준/Silver/2622. 삼각형만들기/삼각형만들기.py
@@ -0,0 +1,12 @@
+n = int(input())
+cnt = 0
+for i in range(1, n+1):
+    for j in range(i, n+1):
+        k = n - i - j
+        if k >= i + j:
+            continue
+        else:
+            if j > k:
+                break
+            cnt += 1
+print(cnt)
